f473e636475acaf9df2716cfa776aa27516a18f3
mbehzad <m_online_net@yahoo.com>
2021-11-14 10:35:22 +0100

    update dependencies (webpack v5.64.0,  webpack-dev-server 4.5.0

--- dist/common.js
+++ dist/common.js
@@ -1,14 +1,21 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["common"],{
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkweboack_dev_server_bug"] = self["webpackChunkweboack_dev_server_bug"] || []).push([["common"],{
 
 /***/ "./src/utils.js":
 /*!**********************!*\
   !*** ./src/utils.js ***!
   \**********************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\nconsole.log(\"###############    UTIL   ############\")\r\n\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (\"UTIL\");\n\n//# sourceURL=webpack:///./src/utils.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconsole.log(\"###############    UTIL   ############\")\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (\"UTIL\");\n\n//# sourceURL=webpack://weboack-dev-server-bug/./src/utils.js?");
 
 /***/ })
 
